c55e40671c48316cade1e1b1667d47e76da79532
Artem Kartasov <agneum@gmail.com>
2017-04-13 19:53:50 +0700

    Go codelab. LRU cache

--- storage/lru/lru.go
+++ storage/lru/lru.go
@@ -39,16 +39,59 @@ func (l *LRU) Add(key, value interface{}) bool {
 	l.items[key] = entry
 	evict := l.evictList.Len() > l.size
 	if evict {
-		l.removeOldest()
+		l.RemoveOldest()
 	}
 	return evict
 }
 
-func (l *LRU) removeOldest() {
+func (l *LRU) Get(key interface{}) (value interface{}, ok bool) {
+	if ent, ok := l.items[key]; ok {
+		l.evictList.MoveToFront(ent)
+		return ent.Value.(*entry).value, true
+	}
+	return
+}
+
+func (l *LRU) GetOldest() (interface{}, interface{}, bool) {
+	ent := l.evictList.Back()
+	if ent != nil {
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+func (l *LRU) Keys() []interface{} {
+	keys := make([]interface{}, len(l.items))
+	i := 0
+	for ent := l.evictList.Back(); ent != nil; ent = ent.Prev() {
+		keys[i] = ent.Value.(*entry).key
+		i++
+	}
+	return keys
+}
+
+func (l *LRU) Contains(key interface{}) (ok bool) {
+	_, ok = l.items[key]
+	return ok
+}
+
+func (l *LRU) Remove(key interface{}) bool {
+	if ent, ok := l.items[key]; ok {
+		l.removeElement(ent)
+		return true
+	}
+	return false
+}
+
+func (l *LRU) RemoveOldest() (interface{}, interface{}, bool) {
 	ent := l.evictList.Back()
 	if ent != nil {
 		l.removeElement(ent)
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
 	}
+	return nil, nil, false
 }
 
 func (l *LRU) removeElement(e *list.Element) {
@@ -59,4 +102,10 @@ func (l *LRU) removeElement(e *list.Element) {
 
 func (l *LRU) Len() int {
 	return l.evictList.Len()
+}
+
+func (l *LRU) Purge() {
+	for k := range l.items {
+		delete(l.items, k)
+	}
 }
--- storage/lru/lru_test.go
+++ storage/lru/lru_test.go
@@ -1,6 +1,10 @@
 package lru
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestLRU_add(t *testing.T) {
 	l, err := New(1)
@@ -14,5 +18,107 @@ func TestLRU_add(t *testing.T) {
 
 	if l.Add(2, 2) == false {
 		t.Errorf("should have an eviction")
+	}
+}
+
+func TestLRU_contains(t *testing.T) {
+	l, err := New(2)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	l.Add(1, 1)
+	l.Add(2, 2)
+
+	if !l.Contains(1) {
+		t.Errorf("1 should be contained")
+	}
+
+	l.Add(3, 3)
+	if l.Contains(1) {
+		t.Errorf("contains should not have updated recent-ness of 1")
+	}
+}
+
+func TestLRU_GetOldest_RemoveOldest(t *testing.T) {
+	l, err := New(128)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	for i := 0; i < 256; i++ {
+		l.Add(i, i)
+	}
+
+	k, _, ok := l.GetOldest()
+	if !ok {
+		t.Fatalf("missing element")
+	}
+	if k.(int) != 128 {
+		t.Fatalf("bad value: %v", k)
+	}
+
+	k, _, ok = l.RemoveOldest()
+	if !ok {
+		t.Fatalf("missing element")
+	}
+	if k.(int) != 128 {
+		t.Fatalf("bad value: %v", k)
+	}
+
+	k, _, ok = l.RemoveOldest()
+	if !ok {
+		t.Fatalf("missing element")
+	}
+	if k.(int) != 129 {
+		t.Fatalf("wrong value: %v", k)
+	}
+}
+
+func TestLRU(t *testing.T) {
+	l, err := New(128)
+	assert.NoError(t, err)
+
+	for i := 0; i < 256; i++ {
+		l.Add(i, i)
+	}
+	assert.Equal(t, 128, l.Len())
+
+	for i, k := range l.Keys() {
+		if v, ok := l.Get(k); !ok || k != v || v != i+128 {
+			t.Fatalf("wrong key: %v", k)
+		}
+	}
+
+	for i := 0; i < 128; i++ {
+		_, ok := l.Get(i)
+		assert.False(t, ok)
+	}
+	for i := 128; i < 256; i++ {
+		_, ok := l.Get(i)
+		assert.True(t, ok)
+	}
+	for i := 128; i < 192; i++ {
+		ok := l.Remove(i)
+		assert.True(t, ok)
+		ok = l.Remove(i)
+		assert.False(t, ok)
+		_, ok = l.Get(i)
+		assert.False(t, ok)
+	}
+
+	l.Get(192)
+
+	for i, k := range l.Keys() {
+		if (i < 63 && i != i+193) || (i == 63 && k != 192) {
+			t.Fatalf("out of order key: %v", k)
+		}
+	}
+
+	l.Purge()
+	if l.Len() != 0 {
+		t.Fatalf("wrong len: %v", l.Len())
+	}
+	if _, ok := l.Get(200); ok {
+		t.Fatalf("should contain nothing")
 	}
 }
